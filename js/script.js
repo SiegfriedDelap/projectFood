bcb4a7edb8f8bd909af859bfbf8dddc108c92d21
Pablo <plaptewww@gmail.com>
2021-03-09 15:05:21 +0200

    XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -282,4 +282,55 @@ window.addEventListener('DOMContentLoaded', () => {
         
     ).render();
 
+    //Отправка данных на вебсервер
+
+    const forms = document.querySelectorAll('form');
+
+    //Ошибки сообщения, нужно поместить на страницу
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо! Сы скоро с вами свяжемся!',
+        failure:'Что-то пошло не так...'
+    };
+
+    forms.forEach(item =>{
+        postData(item);
+    }); //на каждые данные отправляем 
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value,key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 20000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
