2feff4dffb6b9fe24012814ccd5f8b90e2cad010
Pablo <plaptewww@gmail.com>
2021-03-15 14:23:13 +0200

    fetch API formData

--- js/script.js
+++ js/script.js
@@ -310,29 +310,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
             form.insertAdjacentElement('afterend', statusMessage);
         
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value,key){
-                object[key] = value;
-            });
+            // const object = {};
+            // formData.forEach(function(value,key){
+            //     object[key] = value;
+            // });
 
-            const json = JSON.stringify(object);
+            // const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.succes);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+            fetch('server.php', {
+                method: "POST",
+                // headers: {
+                //     'Content-type': 'application/json'
+                // },
+                body: formData
+           }).then(data =>data.text())
+             .then(data => {
+                 console.log(data);
+                 showThanksModal(message.succes);
+                 statusMessage.remove();
+           }).catch(()=>{
+                 showThanksModal(message.failure);
+           }).finally(()=>{
+                 form.reset();
+           });
         });
     }
 
@@ -371,5 +373,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
 
     }
+
+ 
 
 });
