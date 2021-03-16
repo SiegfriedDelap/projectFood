2ffa6ee4cefec34e90c6dd74e6a0465a97d495e0
Pablo <plaptewww@gmail.com>
2021-03-16 20:47:50 +0200

    added some files and little db.json

--- js/script.js
+++ js/script.js
@@ -373,6 +373,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
 
- 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res=> console.log(res));
 
 });
